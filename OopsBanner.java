1e2773f07198c3d493657bb23fb7a80f86faa58e
Rohithkumar-14 <rohithkumar140607@gmail.com>
2026-02-10 22:46:33 +0530

    Separate UC1 and UC2 outputs in dev branch

--- OopsBanner.java
+++ OopsBanner.java
@@ -1,11 +1,5 @@
 class OopsBanner {
     public static void main(String[] args) {
-        System.out.println("  ***       ***      ****      **** ");
-        System.out.println(" *   *     *   *     *   *    *     ");
-        System.out.println("*     *   *     *    ****      **** ");
-        System.out.println("*     *   *     *    *            * ");
-        System.out.println("*     *   *     *    *            * ");
-        System.out.println(" *   *     *   *     *        *   * ");
-        System.out.println("  ***       ***      *         ***  ");
+        System.out.println("OOPS");
     }
 } 
