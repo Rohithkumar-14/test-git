415ba129ea8ade322f7bb3e4b210c8480be0a9e0
Rohithkumar-14 <rohithkumar140607@gmail.com>
2026-02-20 13:05:54 +0530

    UC5: Optimized array initialization using String.join and enhanced for loop

--- Uc4.java
+++ Uc4.java
@@ -1,17 +1,22 @@
 public class Uc4 {
+
     public static void main(String[] args) {
 
-        String[] bannerLines = {
-            String.join("", "  ***       ***      ", "****      ", "****"),
-            String.join("", " *   *     *   *     ", "*   *    ", "*"),
-            String.join("", "*     *   *     *    ", "****      ", "****"),
-            String.join("", "*     *   *     *    ", "*            ", "*"),
-            String.join("", "*     *   *     *    ", "*            ", "*"),
-            String.join("", " *   *     *   *     ", "*        ", "*   *"),
-            String.join("", "  ***       ***      ", "*         ", "***")
+        
+        String[] lines = {
+
+            String.join("  ", " ***** ", " ***** ", " ***** ", " ***** "),
+            String.join("  ", "*     *", "*     *", "*     *", "*      "),
+            String.join("  ", "*     *", "*     *", "*     *", "*      "),
+            String.join("  ", "*     *", "*     *", " ***** ", " ***** "),
+            String.join("  ", "*     *", "*     * ","*      ", "      *"),
+            String.join("  ", "*     *", "*     * ","*      ", "      *"),
+            String.join("  ", " ***** ", " *****  ","*      ",  "******")
+
         };
 
-        for (String line : bannerLines) {
+        
+        for (String line : lines) {
             System.out.println(line);
         }
     }
